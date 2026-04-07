40b69a4bca32c7a1de2012e5b79ed250ff44ecfa
Lakshmi <bandaru.lakshmi.s.134@gmail.com>
2026-04-07 13:20:27 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ status: 'ok' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'not found' }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
